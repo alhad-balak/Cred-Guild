d9bb35977bea3b35fdd64311dbe270abe3380b4f
Abhinav Kumar Gupta <84512702+alhad-balak@users.noreply.github.com>
2022-04-29 20:07:49 +0530

    HALF mAIN DONE

--- src/components/MainBody/OtherBody.js
+++ src/components/MainBody/OtherBody.js
@@ -7,34 +7,52 @@ import item3 from './item-3.webp'
 import item4 from './item-4.png'
 import item5 from './item-5.png'
 import item6 from './item-6.png'
+import cyball from './cyball.png'
 
 const OtherBody = () => {
     return (
-        <div className="grid">
+        <div className="main-grid">
             <div className="item-1 grid-item" id="item-1">
-                <div className="text" style={{ backgroundImage: `url(${item1bg})` }}>
-                    <div className="outline-text">GET SET, PLAY,</div><div className="fill-text">EARN</div>
-                    <div>We are a guild of gamers who are passionate about P2E. We aim to empower our guild members to enjoy all the benefits as they explore the vast emergent socialfi ecosystem.</div>
-                    <a href="/">JOIN OUR GUILD NOW</a>
+                <div className="text">
+                    <div><p className=" text-title outline-text" >GET SET,</p> <p className="text-title outline-text">PLAY,</p><p className="text-title fill-text">EARN</p></div>
+                    <div className='text-des'>We are a guild of gamers who are passionate about P2E. We aim to empower our guild members to enjoy all the benefits as they explore the vast emergent socialfi ecosystem.</div>
+                    <a href="/" ><button className='join-guild'>JOIN OUR GUILD NOW</button></a>
                 </div>
-                <div className="image" style={{ backgroundImage: `url(${item1bg})`, transform: `rotate(180deg)` }}>
-                    <img src={item1} alt="" style={{ transform: `rotate(180deg)` }} />
+                <div className="image" >
+                    <img id="item-1" src={item1} alt="" />
                 </div>
             </div>
             <div className="item-2 grid-item">
-                <div className="image">
-                    <img src={item2} alt="" />
+                <div className="image" >
+                    <img src={item2} alt="" className="item-2-pic" />
                 </div>
                 <div className="text">
+                    <div ><p className=" text-title outline-text item-2-title" >YOUR</p> <p className="item-2-title text-title outline-text">ADVENTURE</p><p className="item-2-title text-title fill-text">STARTS NOW</p></div><div className="image" >
+                        <img src={item2} alt="" className="item-22-pic" />
+                    </div>
 
+                    <div className='text-des item-2-des'>We currently owned 1000+ gaming NFTs and will add more in the future. You are free to explore each world according to your own tactics and gameplay.</div>
+                    <div style={{ color: '#4e27af' }}><strong>All our members have full access to our DAO inventory of NFTs, and also a step by step guide on how to start P2E.</strong></div>
+                    <div id='item-2-text-grid'>
+                        <img src={cyball} alt="" className='item-2-text-grid-content' />
+                        <img src={cyball} alt="" className='item-2-text-grid-content' />
+                        <img src={cyball} alt="" className='item-2-text-grid-content' />
+                        <img src={cyball} alt="" className='item-2-text-grid-content' />
+                    </div>
                 </div>
             </div>
             <div className="item-3 grid-item">
-                <div className="image">
-                    <img src={item3} alt="" />
-                </div>
                 <div className="text">
+                    <div ><p className=" text-title outline-text item-2-title" >MINT YOUR</p><p className="item-2-title text-title fill-text">GUILD BADGE</p></div><div className="image" >
+                        <img src={item3} alt="" className="item-22-pic" />
+                    </div>
 
+                    <div className='text-des item-2-des item-3-des'>You can enjoy our inventory of NFTs and other boons with yout RANKER Badge. Exclusive NFTs, Tournaments, coaching sessions and beyond.</div>
+
+                    <a href="/" ><button className='join-guild'>LEARN ABOUT $RANKER</button></a>
+                </div>
+                <div className="image" >
+                    <img src={item3} alt="" className="item-2-pic item-3-pic" />
                 </div>
             </div>
             <div className="item-4 grid-item">
@@ -62,7 +80,7 @@ const OtherBody = () => {
 
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
