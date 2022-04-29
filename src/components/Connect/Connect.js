80ab4ec2c76d9abc894c6575cd8fba21754234ed
Abhinav Kumar Gupta <84512702+alhad-balak@users.noreply.github.com>
2022-04-29 17:14:46 +0530

    Footer completed

--- src/components/Connect/Connect.js
+++ src/components/Connect/Connect.js
@@ -5,7 +5,22 @@ import cred from './cred.png'
 const Connect = () => {
     return (
         <div className='connect'>
-connect
+            <div className="container">
+                <div id="col-1">
+                    <div id="logo"><img src={cred} alt="" /></div>
+                    <div id="rights">© RankerDao. All Rights Reserved 2022</div>
+                </div>
+                <div id="col-2">
+                    <a href="/">TELEGRAM</a>
+                    <a href="/">TWITTER</a>
+                    <a href="/">DISCORD</a>
+                    <a href="/">INSTAGRAM</a>
+                    <a href="/">INVENTORY</a>
+                    <a href="/">$RANKER</a>
+                    <a href="/">MINT BADGE</a>
+                    <a href="/">LITE PAPER</a>
+                </div>
+            </div>
         </div>
     )
 }
